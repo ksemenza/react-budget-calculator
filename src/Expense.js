3bf46540626afd678629a025bb02c96cded78169
Ted Wu <ted.wu@chartboost.com>
2016-12-09 17:19:09 -0800

    added calc sum function

--- src/Expense.js
+++ src/Expense.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class Expense extends Component {
+    constructor() {
+        super();
+        this.state = {
+            essentialsTotal: 0,
+            wantsTotal: 0,
+            savingsTotal: 0
+        };
+    }
+    addSum(event) {
+        console.log(event.target.value);
+        this.setState({
+            essentialsTotal: 
+            wantsTotal:
+            savingsTotal:
+        })
+    }
     render() {
         const categoryHeaderClass = "category__header--" + this.props.category;
         return (
@@ -10,6 +26,7 @@ class Expense extends Component {
               <input className={categoryHeaderClass}
                      type="text"
                      placeholder={this.props.expense}
+                     onChange={this.addSum}
               />
             </div>
           </div>

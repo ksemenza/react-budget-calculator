ff8f52dfc4cb91df9da94da41f55b9fed2e33501
Ted Wu <ted.wu@chartboost.com>
2017-02-01 21:47:02 -0800

    Tweaked the financial equation

--- src/finance.js
+++ src/finance.js
@@ -1,8 +1,24 @@
-//Calculating futureValue with compounding interest
+// //Calculating futureValue with compounding interest
 
-export function compoundInterest( rate, principal, numOfPeriods) {
-    var CI = principal * Math.pow((1.00 + rate), numOfPeriods);
-    return Math.round(CI * 100) / 100;
+// export function compoundInterest( rate, principal, numOfPeriods) {
+//     var CI = principal * Math.pow((1.00 + rate), numOfPeriods);
+//     return Math.round(CI * 100) / 100;
+// }
+
+/*
+    This returns the future value after compounding interest
+    @params startBalance - starting balance
+    @params yearsToGrow - number of years to compound
+    @params monthlyContribution - monthly contribution in dollars
+    @params rate - interest rate
+    @returns compounding interest
+ */
+
+export const compoundInterest = (rate, startBalance, yearsToGrow, monthlyContribution) => {
+    let monthlyRate = rate / 12;
+    let monthsToGrow = yearsToGrow * 12;
+    for(var i = 0; i < monthsToGrow; i++) {
+      startBalance = startBalance * (1 + monthlyRate) + monthlyContribution;
+    }
+    return Math.round(startBalance * 100) / 100;
 }
-
-//FV = PV (1 +i/n) nt
